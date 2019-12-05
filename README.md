4b68e8bc272c6c5012b033ab8a28cc93ad4101cc
DJ Krummen <44108933+krummenCode@users.noreply.github.com>
2019-12-05 12:14:27 -0600

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# React-To-Do-App
-## [Live Demo](http://krummenCode.github.io/React-To-Do-App)
-
-React app that allows users to enter "To Do" items, mark them as complete and delete the items.
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-To-Do-App/precache-manifest.65ffbbadeffc49ae9bef7d072f26cc72.js"
+  "/precache-manifest.82e2c08ba36beafb2e69ccc68e440b92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-To-Do-App/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
